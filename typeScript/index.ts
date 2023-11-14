e75ecfbbb26ea5861f332bbab90f1fdb08f24494
Vingren <132018575+VingrenSousa@users.noreply.github.com>
2023-11-13 21:42:55 -0300

    next

--- typeScript/index.ts
+++ typeScript/index.ts
@@ -4,6 +4,7 @@ const porta = 3000;
 const serve = https.createServer((requeste:any, resposta:any) => {
     resposta.writeHead(200, {"Content-Type": "text/html" });
     resposta.write("<h2>ola mundo</h2>");
+    resposta.write("<p>"+requeste.url+"</p>")
     resposta.end()
 }).listen(3000);
 serve;
